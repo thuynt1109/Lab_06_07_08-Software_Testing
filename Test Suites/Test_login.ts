e1dbf4453b462bf5d884e21ba8724aef2daeedad
thuynt <thuybinh11997@gmail.com>
2020-09-24 23:53:33 +0700

    Lab1-Software_Testing

---
+++ Test Suites/Test_login.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test_login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>7495dfe8-9ccb-4401-b443-f08a102c6a57</testSuiteGuid>
+</TestSuiteEntity>
